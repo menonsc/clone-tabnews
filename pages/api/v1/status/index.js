89a14bdf3beabd4c4e94d0f44306d60d3555f157
menonsc <joaomenonsc@gmail.com>
2025-05-09 13:52:22 +0000

    makes `database.js` `client.connect()` more robust to errors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -20,8 +20,6 @@ async function status(request, response) {
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
 
-  console.log(databaseOpenedConnectionsValue);
-
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
